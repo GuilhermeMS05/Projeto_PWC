f653d11fa1a78aaaef027674bd00f307580c76ed
Guilherme Silva <guilhermemendessilva123@gmail.com>
2025-01-04 16:30:35 +0000

    conclusão página favoritos

--- assets/js/paises.js
+++ assets/js/paises.js
@@ -16,31 +16,32 @@ $(document).on("click", "#favorite", function () {
 </svg>`;
 
   const countryCard = $(this).closest(".card");
-  const countryID = countryCard.find("a").attr("href");
+  const countryURL = countryCard.find("a").attr("href");
   const countryName = countryCard.find(".card-title").text();
   const countryFlag = countryCard.find("img").attr("src");
 
   const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  
+
   if ($(this).html().includes("bi-star-fill")) {
     $(this).html(starEmpty);
 
-    const updatedFavorites = favorites.filter((fav) => fav.name !== countryName);
+    const updatedFavorites = favorites.filter(
+      (fav) => fav.name !== countryName
+    );
     localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
   } else {
     $(this).html(starFilled);
 
-    favorites.push({ id: countryID, name: countryName, flag: countryFlag });
+    favorites.push({ url: countryURL, name: countryName, flag: countryFlag });
     localStorage.setItem("favorites", JSON.stringify(favorites));
   }
-
 });
 
 function fetchCountries(inputText) {
   var api_url = "https://restcountries.com/v3.1/all";
 
   if (inputText) {
-    // Alteração para filtrar a busca pelo nome do país.
+    // Alteração para filtrar a busca pelo nome do país
     api_url = "https://restcountries.com/v3.1/name/" + inputText.toLowerCase();
   }
 
@@ -78,5 +79,20 @@ function displaycountries(arrayPaises) {
                             </div>
                         </div>`;
     listapaises.append(paisCard);
+  });
+
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+  // Percorre todas as estrelas e verifica se estão nos favoritos
+  $(".card").each(function () {
+    const countryName = $(this).find(".card-title").text();
+    const favorite = favorites.find((fav) => fav.name === countryName);
+
+    if (favorite) {
+      $(this).find("#favorite")
+        .html(`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+</svg>`);
+    }
   });
 }
